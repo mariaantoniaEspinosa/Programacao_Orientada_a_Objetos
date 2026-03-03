df24e5b70675a5cd68487f94c533e574ab429153
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-03-03 08:56:41 -0300

    Create Exercício08.java

---
+++ Exercícios_de_Aula/Aula02/Exercício08.java
@@ -0,0 +1,21 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Exercício08 {
+
+	public static void main(String[] args) {
+		//  Escreva um programa Java que use um laço para ler vários nomes de cidades do usuário até que o nome "São Paulo" seja lido.
+		Scanner teclado = new Scanner (System.in);
+		
+		String cidade;
+		
+		do {
+			System.out.println("Digite o nome de uma cidade: ");
+			cidade = teclado.nextLine();
+		} while(!cidade.equals("Sao Paulo"));
+		
+
+	}
+
+}
