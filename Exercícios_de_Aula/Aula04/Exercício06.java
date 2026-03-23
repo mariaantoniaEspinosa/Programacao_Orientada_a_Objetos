c33f9912b385c696fec5243f261d69e767779599
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-03-23 19:43:25 -0300

    Create Exercício06.java

---
+++ Exercícios_de_Aula/Aula04/Exercício06.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class PrincipalAluno {
+
+	public static void main(String[] args) {
+		 	Aluno a1 = new Aluno("Maria", "12345");
+
+	        Aluno a2 = new Aluno("23/01/2006");
+	        
+	        Aluno a3 = new Aluno("Maria", "23/01/2006", 2024);
+
+	}
+
+}
