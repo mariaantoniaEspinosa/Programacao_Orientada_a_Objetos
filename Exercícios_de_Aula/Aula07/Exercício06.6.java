f3ad0a8e15f2b232aa058bbc439278aa8a3cc704
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-04-13 11:59:23 -0300

    Create Exercício06.6.java

---
+++ Exercícios_de_Aula/Aula07/Exercício06.6.java
@@ -0,0 +1,41 @@
+package pkg;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private int idade;
+	
+	public Pessoa (String nome, int idade, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.idade = idade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public boolean verificarMaiorDeIdade() {
+		return idade >= 18;
+	}
+}
