0bc499e77463630fd641207ca7c6679d42d2ce16
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-04-13 10:39:24 -0300

    Update Exercício02.java

--- Exercícios_de_Aula/Aula07/Exercício02.java
+++ Exercícios_de_Aula/Aula07/Exercício02.java
@@ -7,7 +7,6 @@ public class PrincipalCirculo {
 	public static void main(String[] args) {
 		Circulo c = new Circulo(0);
 		Scanner teclado = new Scanner (System.in);
-		double raio;
 		
 		System.out.println("Digite o valor do raio para calcular a área de um circulo: ");
 		c.setRaio(teclado.nextDouble());
