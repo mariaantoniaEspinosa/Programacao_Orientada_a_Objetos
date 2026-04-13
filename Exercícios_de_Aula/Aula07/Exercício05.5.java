2bbf5367584c78335dcb50ab1e8b7990ef59003a
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-04-13 11:31:39 -0300

    Add Triangulo class with side properties and methods

---
+++ Exercícios_de_Aula/Aula07/Exercício05.5.java
@@ -0,0 +1,39 @@
+package pkg;
+
+public class Triangulo {
+	private double ladoA, ladoB, ladoC;
+	
+	public Triangulo(double ladoA, double ladoB, double ladoC) {
+		this.ladoA = ladoA;
+		this.ladoB = ladoB;
+		this.ladoC = ladoC;
+	}
+	
+	 public double getLadoA() {
+	        return ladoA;
+	 }
+
+	 public void setLadoA(double ladoA) {
+	        this.ladoA = ladoA;
+	 }
+
+	 public double getLadoB() {
+	        return ladoB;
+	 }
+
+	 public void setLadoB(double ladoB) {
+	        this.ladoB = ladoB;
+	 }
+	 
+	 public double getLadoC() {
+	        return ladoC;
+	 }
+
+	 public void setLadoC(double ladoC) {
+	        this.ladoC = ladoC;
+	 }
+	 
+	 public boolean verificarEquilatero() {
+		 return ladoA == ladoB && ladoB == ladoC;
+	 }
+}
