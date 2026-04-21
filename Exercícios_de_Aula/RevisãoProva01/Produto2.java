e64b3ea07071c8fb13466f34a36c6059930fb779
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-04-21 16:24:26 -0300

    Create Produto2.java

---
+++ Exercícios_de_Aula/RevisãoProva01/Produto2.java
@@ -0,0 +1,49 @@
+package pkg;
+
+public class Produto {
+	private String nome;
+	private double preco;
+	private int quantidadeEstoque;
+	
+	 public Produto(String nome) {
+	    this.nome = nome;
+	    this.preco = 0;
+	    this.quantidadeEstoque = 0;
+	 }
+
+	
+	 public Produto(String nome, double preco) {
+	   this.nome = nome;
+	   this.preco = preco;
+	   this.quantidadeEstoque = 0;
+	 }
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public int getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	
+	
+	public void exibirInformacoes() {
+		System.out.println("Nome: " + nome);
+		System.out.println("Preço: " + preco);
+		System.out.println("Quantia Estoque: " + quantidadeEstoque);
+		
+	}
+}
