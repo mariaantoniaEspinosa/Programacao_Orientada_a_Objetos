e6e9a808a06d961248b99fb22dd72b84d98dca18
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-05-02 20:25:14 -0300

    Add Retangulo class with area calculation method

---
+++ Exercícios_de_Aula/Aula10/Exercício01-Retangulo.java
@@ -0,0 +1,11 @@
+package pacote;
+
+public class Retangulo extends Figura{
+	protected double largura;
+	protected double altura;
+	
+	public double calcularArea(){
+		return largura * altura;
+		
+	}
+}
