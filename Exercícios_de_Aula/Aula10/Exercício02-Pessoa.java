df220d2a3d612bf5d432ef62ed4e30aff6220350
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-05-02 20:32:47 -0300

    Create Exercício02-Pessoa.java

---
+++ Exercícios_de_Aula/Aula10/Exercício02-Pessoa.java
@@ -0,0 +1,13 @@
+package pacote2;
+
+public class Pessoa {
+	protected String nome;
+	
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+	
+	public void trabalhar() {
+		System.out.println("A pessoa está trabalhando");
+	}
+}
