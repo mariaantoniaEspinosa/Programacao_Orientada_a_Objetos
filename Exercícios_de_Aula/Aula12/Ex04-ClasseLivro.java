70038044a5cff59b249f79cced4bce20df04aa30
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-05-18 14:26:52 -0300

    Create Ex04-ClasseLivro.java

---
+++ Exercícios_de_Aula/Aula12/Ex04-ClasseLivro.java
@@ -0,0 +1,34 @@
+package pacote1;
+
+public class Livro {
+	protected String titulo;
+	protected String autor;
+	protected int ano;
+	
+	public Livro(String titulo, String autor, int ano) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.ano = ano;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	
+}
