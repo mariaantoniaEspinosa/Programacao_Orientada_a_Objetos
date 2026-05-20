afc8a7c6209e691481aeb3b43d1e260488ee26db
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-05-20 16:23:09 -0300

    Create Ex08-ClasseReserva.java

---
+++ Exercícios_de_Aula/Aula13/Ex08-ClasseReserva.java
@@ -0,0 +1,10 @@
+package pacote8;
+
+public class Reserva {
+	public void adicionar() {
+		System.out.println("Adionando reserva");
+	}
+	public void cancelar() {
+		System.out.println("Cancelando reserva");
+	}
+}
